efb98c2f86a7ade75b7961423e45c978ce68107d
MartinPerezDev <martinperez9713@gmail.com>
2024-03-11 14:53:17 -0300

    feat: add get an Artist

--- controllers/artist.js
+++ controllers/artist.js
@@ -13,7 +13,27 @@ const save = async (req, res) => {
   }
 }
 
+const one = async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const artistDb = await Artist.findById(id)
+    if (!artistDb) return res.status(404).json({ status: "error", message: "Artist not found" })
+
+    return res.status(200).json({ status: "success", message: "Get an artist", artist: artistDb })
+  } catch (error) {
+    return res.status(500).json({ status: "error", message: "Error to get an artist" })
+  }
+}
+
+const list = async (req, res) => {
+  return res.status(200).json({ status: "success", message: "List Artist" })
+
+}
+
 
 module.exports = {
-  save
+  save,
+  one,
+  list
 }
--- routes/artist.js
+++ routes/artist.js
@@ -5,5 +5,7 @@ const check = require("../middlewares/auth")
 const router = express.Router()
 
 router.post("/save", check.auth, ArtistController.save)
+router.get("/one/:id", check.auth, ArtistController.one)
+router.get("/list/:page?", check.auth, ArtistController.list)
 
 module.exports = router
