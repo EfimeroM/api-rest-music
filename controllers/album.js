cb38883411f4af0790623967775420c1ad5d400b
MartinPerezDev <martinperez9713@gmail.com>
2024-03-22 12:01:03 -0300

    feat: add upload image & get image album

--- controllers/album.js
+++ controllers/album.js
@@ -1,4 +1,6 @@
 const Album = require("../models/Album")
+const fs = require("fs")
+const path = require("path")
 
 const save = async (req, res) => {
   const params = req.body
@@ -41,11 +43,11 @@ const list = async (req, res) => {
 }
 
 const update = async (req, res) => {
-  const { albumId } = req.params
+  const { id } = req.params
   const data = req.body
 
   try {
-    const albumUpdated = await Album.findByIdAndUpdate(albumId, data, { new: true })
+    const albumUpdated = await Album.findByIdAndUpdate(id, data, { new: true })
     if (!albumUpdated) return res.status(404).json({ status: "error", message: "Error album not found" })
 
     res.status(200).json({ status: "success", message: "Update album", album: albumUpdated })
@@ -54,9 +56,44 @@ const update = async (req, res) => {
   }
 }
 
+const uploadImage = async (req, res) => {
+  const { id } = req.params
+  if (!req.file) return res.status(404).json({ status: "error", message: "Error file not found" })
+
+  const fileName = req.file.originalname
+  const splitFile = fileName.split(".")
+  const fileExtension = splitFile[1]
+  if (!["png", "jpg", "jpeg", "gif"].includes(fileExtension)) {
+    fs.unlinkSync(req.file.path)
+    return res.status(400).json({ status: "error", message: "Error invalid file" })
+  }
+
+  try {
+
+    const albumUpdated = await Album.findOneAndUpdate({ _id: id }, { image: req.file.filename }, { new: true })
+    if (!albumUpdated) return res.status(400).json({ status: "error", message: "Error updating album image" })
+
+    return res.status(200).json({ status: "success", message: "Image uploaded", album: albumUpdated, file: req.file })
+  } catch (error) {
+    return res.status(500).json({ status: "error", message: "Error updating album image" })
+  }
+}
+
+const image = async (req, res) => {
+  const file = req.params.file
+  const filePath = `./uploads/albums/${file}`
+
+  fs.stat(filePath, (error, exists) => {
+    if (!exists) return res.status(404).json({ status: "error", message: "Image not found", exists, file, filePath })
+    return res.sendFile(path.resolve(filePath))
+  })
+}
+
 module.exports = {
   save,
   one,
   list,
-  update
+  update,
+  uploadImage,
+  image
 }
--- routes/album.js
+++ routes/album.js
@@ -1,12 +1,22 @@
 const express = require("express")
 const AlbumController = require("../controllers/album")
 const check = require("../middlewares/auth")
+const multer = require("multer")
 
 const router = express.Router()
+
+//config multer
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "./uploads/albums"),
+  filename: (req, file, cb) => cb(null, `album-${Date.now()}-${file.originalname}`)
+})
+const uploads = multer({ storage })
 
 router.post("/save", check.auth, AlbumController.save)
 router.get("/one/:id", check.auth, AlbumController.one)
 router.get("/list/:artistId", check.auth, AlbumController.list)
-router.put("/update/:albumId", check.auth, AlbumController.update)
+router.put("/update/:id", check.auth, AlbumController.update)
+router.post("/upload/:id", [check.auth, uploads.single("file0")], AlbumController.uploadImage)
+router.get("/image/:file", AlbumController.image )
 
 module.exports = router
